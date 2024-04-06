7bd19f359cf2e1fe0c157d77c9f1fd9892afae71
“[awaisgulzar]” <[mawaisjani78563@gmail.com]>
2024-04-06 23:57:37 +0500

    initial commit

---
+++ src/Components/header.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+function Header() {
+  return (
+    <main className="flex justify-between bg-slate-400 h-16 content-center">
+      <h1 className="m-4 text-3xl font-bold italic">Jazzi</h1>
+
+      <nav className="m-5 space-x-5 ">
+        <Link className="hover:bg-slate-600 hover:rounded " href={""}>
+          Home
+        </Link>
+        <Link className="hover:bg-slate-600 hover:rounded " href={""}>
+          About
+        </Link>
+        <Link className="hover:bg-slate-600 hover:rounded " href={""}>
+          Contact US
+        </Link>
+        <Link className="hover:bg-slate-600 hover:rounded " href={""}>
+          Services
+        </Link>
+      </nav>
+    </main>
+  );
+}
+
+export default Header;
